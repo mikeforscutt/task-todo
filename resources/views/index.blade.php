fd5801e4c650ac76c5c031fd20738e62be4b99cd
mikeforscutt <mikeforscutt@gmail.com>
2025-05-22 11:38:00 +0100

    style form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,29 +3,22 @@
 @section('title', 'The list of tasks')
 
 @section('content')
+  <nav class="mb-4">
+    <a href="{{ route('tasks.create') }}" class="link">Add Task!</a>
+  </nav>
 
-<nav class="mb-4">
-  <a href="{{ route('tasks.create') }}"
-  class="font-medium text-gray-700 underline decoration-pink-500 hover:text-gray-600">Add Task</a>
-</nav>
-
-  {{-- @if (count($tasks)) --}}
   @forelse ($tasks as $task)
     <div>
       <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
-        @class(['line-through' => $task->completed])>
-        {{ $task->title }}
-      </a>
+        @class(['line-through' => $task->completed])>{{ $task->title }}</a>
     </div>
   @empty
     <div>There are no tasks!</div>
   @endforelse
-  {{-- @endif --}}
 
   @if ($tasks->count())
-  <nav class="mt-4">
-    {{ $tasks->links() }}
-  </nav>
+    <nav class="mt-4">
+      {{ $tasks->links() }}
+    </nav>
   @endif
-
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 10 Task List App</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    @yield('styles')
+  <title>Laravel 10 Task List App</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  {{-- blade-formatter-disable --}}
+  <style type="text/tailwindcss">
+    .btn {
+      @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+    }
+
+    .link {
+      @apply font-medium text-gray-700 underline decoration-pink-500
+    }
+
+    label {
+      @apply block uppercase text-slate-700 mb-2
+    }
+
+    input, 
+    textarea {
+      @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+    }
+
+    .error {
+      @apply text-red-500 text-sm
+    }
+  </style>
+  {{-- blade-formatter-enable --}}
+
+  @yield('styles')
 </head>
+
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
-    <h1 class="text-2xl mb-4">@yield('title')</h1>
-    <div>
-        @if (session()->has('success'))
-            <p>{{ session('success') }}</p>
-        @endif
-        @yield('content')
-    </div>
+  <h1 class="mb-4 text-2xl">@yield('title')</h1>
+  <div>
+    @if (session()->has('success'))
+      <div>{{ session('success') }}</div>
+    @endif
+
+    @yield('content')
+  </div>
 </body>
+
 </html>
